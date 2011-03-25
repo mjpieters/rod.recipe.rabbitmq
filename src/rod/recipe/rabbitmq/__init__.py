823cbc114f6d5b63d74738b1239012f1f4e7dc1e
Martijn Pieters <mj@zopatista.com>
2011-03-25 13:30:56 +0100

    Use the download cache if available

--- src/rod/recipe/rabbitmq/__init__.py
+++ src/rod/recipe/rabbitmq/__init__.py
@@ -153,9 +153,14 @@ exec %(erl)s \\
         arch_filename = self.options['url'].split(os.sep)[-1]
         dst = os.path.join(self.buildout['buildout']['parts-directory'],
                            self.name)
-        downloads_dir = os.path.join(os.getcwd(), 'downloads')
-        if not os.path.isdir(downloads_dir):
-            os.mkdir(downloads_dir)
+        # Re-use the buildout download cache if defined
+        downloads_dir = self.buildout['buildout'].get('download-cache')
+        if downloads_dir is None:
+            download_dir = os.path.join(self.buildout['buildout']['directory'],
+                                        'downloads')
+            if not os.path.isdir(download_dir):
+                os.mkdir(download_dir)
+            self.buildout['buildout'].setdefault('download-cache', download_dir)
         src = os.path.join(downloads_dir, arch_filename)
         if not os.path.isfile(src):
             logger.info("downloading RabbitMQ distribution...")
